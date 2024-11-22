f09443f49ec3800cf29c663b9dcddf4f1d9d664b
sina-dezhbord <sinadezhbord@gmail.com>
2024-11-22 20:29:33 +0330

    v1.1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
-'use strict'
-let myDiv=document.querySelector(".list")
-let btn = document.querySelector(".btn")
-let ul = document.createElement("ul")
-myDiv.append(ul)
+"use strict";
+let myDiv = document.querySelector(".list");
+let btn = document.querySelector(".btn");
+let ul = document.createElement("ul");
+myDiv.append(ul);
 
-btn.addEventListener("click", ()=>{
-    let li=document.createElement("li")
-    li.innerHTML=prompt("chi")
-    let where=prompt("where?(prepend=ghabl or append=baad)")
-    if(where in li){
-        ul[where](li)
-    }else{
-        alert("not found 404")
-    }
-})
-
+btn.addEventListener("click", () => {
+  let li = document.createElement("li");
+  li.innerHTML = prompt("متن خود را وارد کنید");
+  let where = prompt("(فقط 'قبل' یا 'بعد' بگید)مکانش کجا باشه؟");
+  let flag = false
+  if (where == "بعد") {
+    ul.append(li)
+    flag = true
+  } else if (where == "قبل") {
+    ul.prepend(li)
+    flag = true
+  }else{
+    flag = false
+  }
+  if (flag == true) {
+   alert("success")
+  } else {
+    alert("not found 404");
+  }
+});
